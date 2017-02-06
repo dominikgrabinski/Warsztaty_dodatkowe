1b8f866d705e55e1fde55175827f19947f30185a
dominikgrabinski <grabinski.dominik@gmail.com>
2017-02-06 15:46:58 +0100

    sprwadzenie_User

---
+++ class/User.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * Class reprezent one user
+ * TODO Validate seter
+ * 
+ */
+
+class User {
+    private $id;
+    private $address;
+    private $name;
+    private $surname;
+    private $credits;
+    private $hashedPassword;
+    
+    public function __construct() {
+        $this->id = -1;
+        $this->address = '';
+        $this->name = '';
+        $this->surname = '';
+        $this->credits = null;
+        $this->hashedPassword = '';
+    }
+    
+    public function getId(){
+        return $this->id;
+    }
+    
+    public function getAddress() {
+        return $this->address;
+    }
+    
+    public function setAddress($address){
+        $this->address = $address;
+        return true;
+    }
+    
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function setName($name){
+        $this->name = $name;
+        return true;
+    }
+    
+    public function getSurname(){
+        return $this->surname;
+    }
+    
+    public function setSurname($surname){
+        $this->surname = $surname;
+        return true;
+    }
+    
+        public function getCredits(){
+        return $this->credits;
+    }
+       
+    public function setCredits($ammountCredits){
+        $this->credits = $ammountCredits;
+    }
+    
+    public function getHashedPassword(){
+        return $this->hashedPassword;
+    }
+    
+    public function setHashedPassword($password){
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $this->hashedPassword = $hashedPassword;
+        return true;
+    }
+    
+}
+
